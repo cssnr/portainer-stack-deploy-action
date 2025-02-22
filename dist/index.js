3bb22f80ed70b77acdc038981fdb173af9c84c20
Shane <6071159+smashedr@users.noreply.github.com>
2025-02-21 20:25:43 -0800

    Split Inputs

--- dist/index.js
+++ dist/index.js
@@ -41706,60 +41706,29 @@ const Portainer = __nccwpck_require__(1055)
 
 ;(async () => {
     try {
-        // console.log('github.context:', github.context)
+        core.info('🏳️ Portainer Stack Deploy Action')
+
+        // Parse Inputs
         const { owner, repo } = github.context.repo
+        const inputs = parseInputs(owner, repo)
+        core.startGroup('Parsed Inputs')
+        console.log('inputs:', inputs)
+        core.endGroup() // Inputs
 
-        const token = core.getInput('token', { required: true })
-        // console.log('token:', token)
-        const url = core.getInput('url', { required: true })
-        console.log('url:', url)
-        const name = core.getInput('name', { required: true })
-        console.log('name:', name)
-        const composeFile = core.getInput('file', { required: true })
-        console.log('composeFile:', composeFile)
-        let endpointID = core.getInput('endpoint')
-        console.log('endpointID:', endpointID)
-        const repositoryReferenceName =
-            core.getInput('ref') || github.context.ref
-        console.log('repositoryReferenceName:', repositoryReferenceName)
-        const repositoryURL =
-            core.getInput('repo') || `https://github.com/${owner}/${repo}`
-        console.log('repositoryURL:', repositoryURL)
-        const tlsskipVerify = core.getBooleanInput('tlsskip')
-        console.log('tlsskipVerify:', tlsskipVerify)
-        const prune = core.getBooleanInput('prune')
-        console.log('prune:', prune)
-        const pullImage = core.getBooleanInput('pull')
-        console.log('pullImage:', pullImage)
-        const type = core.getInput('type')
-        console.log('type:', type)
-        if (!['repo', 'file'].includes(type)) {
-            core.setFailed(`Unknown type: ${type}. Must be repo or file.`)
+        if (!['repo', 'file'].includes(inputs.type)) {
+            core.setFailed(`Unknown type: ${inputs.type}. Values: [repo, file]`)
+            return
         }
-        const standalone = core.getBooleanInput('standalone')
-        console.log('standalone:', standalone)
-        const env_json = core.getInput('env_json')
-        // console.log('env_json:', env_json)
-        const env_file = core.getInput('env_file')
-        // console.log('env_file:', env_file)
-        const env = getEnv(env_json, env_file)
-        // console.log('env:', env)
-        let repositoryUsername = core.getInput('username')
-        // console.log('repositoryUsername:', repositoryUsername)
-        let repositoryPassword = core.getInput('password')
-        // console.log('repositoryPassword:', repositoryPassword)
-        const repositoryAuthentication = !!(
-            repositoryUsername || repositoryPassword
-        )
-        console.log('repositoryAuthentication:', repositoryAuthentication)
-        let fs_path = core.getInput('fs_path')
-        console.log('fs_path:', fs_path)
 
-        const portainer = new Portainer(url, token)
+        // Set Variables
+        const newEnv = getEnv(inputs.env_json, inputs.env_file)
+        const repoAuth = !!(inputs.username || inputs.password)
+        const portainer = new Portainer(inputs.url, inputs.token)
 
-        if (fs_path) {
+        if (inputs.fs_path) {
             // get system info and check if portainer is BE edition
             const version = await portainer.getVersion()
+            // console.log('version:', version)
             const is_portainer_be = version.ServerEdition === 'EE'
             if (!is_portainer_be) {
                 return core.setFailed(
@@ -41768,48 +41737,50 @@ const Portainer = __nccwpck_require__(1055)
             }
         }
 
-        if (!endpointID) {
+        if (!inputs.endpointID) {
             const endpoints = await portainer.getEndpoints()
             // console.log('endpoints:', endpoints)
-            endpointID = endpoints[0]?.Id
-            console.log('endpointID:', endpointID)
-            if (!endpointID) {
+            inputs.endpointID = endpoints[0]?.Id
+            core.info(`endpointID: \u001b[36m${inputs.endpointID}`)
+            if (!inputs.endpointID) {
                 return core.setFailed('No Endpoints Found!')
             }
         }
 
         let swarmID = null
-        if (!standalone) {
-            const swarm = await portainer.getSwarm(endpointID)
+        if (!inputs.standalone) {
+            const swarm = await portainer.getSwarm(inputs.endpointID)
             // console.log('swarm:', swarm)
             swarmID = swarm.ID
-            console.log('swarmID:', swarmID)
+            core.info(`swarmID: \u001b[36m${swarmID}`)
         }
 
+        // Get Stack
         const stacks = await portainer.getStacks()
         // console.log('stacks:', stacks)
-        let stack = stacks.find((item) => item.Name === name)
+        let stack = stacks.find((item) => item.Name === inputs.name)
         // console.log('stack:', stack)
         let stackID = stack?.Id
-        console.log('stackID:', stackID)
+        core.info(`stackID: \u001b[36m${stackID}`)
 
-        if (type === 'repo') {
+        // Perform Deploy
+        if (inputs.type === 'repo') {
             core.info('Performing Repository Deployment.')
             if (stackID) {
                 core.info(`Stack Found - Updating Stack ID: ${stack.Id}`)
                 const body = {
-                    env,
-                    prune,
-                    pullImage,
-                    repositoryReferenceName,
-                    repositoryAuthentication,
-                    repositoryPassword,
-                    repositoryUsername,
+                    env: newEnv,
+                    prune: inputs.prune,
+                    pullImage: inputs.pull,
+                    repositoryReferenceName: inputs.ref,
+                    repositoryAuthentication: repoAuth,
+                    repositoryPassword: inputs.password,
+                    repositoryUsername: inputs.username,
                 }
                 // console.log('body:', body)
                 stack = await portainer.updateStackRepo(
                     stackID,
-                    endpointID,
+                    inputs.endpointID,
                     body
                 )
                 // console.log('stack:', stack)
@@ -41817,42 +41788,41 @@ const Portainer = __nccwpck_require__(1055)
             } else {
                 core.info('Stack NOT Found - Deploying NEW Stack')
                 const body = {
-                    name,
+                    name: inputs.name,
                     swarmID,
-                    repositoryURL,
-                    composeFile,
-                    env,
-                    tlsskipVerify,
-                    repositoryReferenceName,
-                    repositoryAuthentication,
-                    repositoryPassword,
-                    repositoryUsername,
-                    // If fs_path is set, add it to the body
-                    ...(fs_path && {
+                    repositoryURL: inputs.repo,
+                    composeFile: inputs.file,
+                    env: newEnv,
+                    tlsskipVerify: inputs.tlsskip,
+                    repositoryReferenceName: inputs.ref,
+                    repositoryAuthentication: repoAuth,
+                    repositoryPassword: inputs.password,
+                    repositoryUsername: inputs.username,
+                    ...(inputs.fs_path && {
                         supportRelativePath: true,
-                        fileSystemPath: fs_path,
+                        fileSystemPath: inputs.fs_path,
                     }),
                 }
                 // console.log('body:', body)
-                stack = await portainer.createStackRepo(endpointID, body)
+                stack = await portainer.createStackRepo(inputs.endpointID, body)
                 // console.log('stack:', stack)
                 core.info(`Deployed Stack: ${stack.Id}: ${stack.Name}`)
             }
-        } else if (type === 'file') {
+        } else if (inputs.type === 'file') {
             core.info('Performing Stack File Deployment.')
-            const stackFileContent = fs.readFileSync(composeFile, 'utf-8')
+            const stackFileContent = fs.readFileSync(inputs.file, 'utf-8')
             if (stackID) {
                 core.info(`Stack Found - Updating Stack ID: ${stackID}`)
                 const body = {
-                    env,
-                    prune,
-                    pullImage,
+                    env: newEnv,
+                    prune: inputs.prune,
+                    pullImage: inputs.pull,
                     stackFileContent,
                 }
                 // console.log('body:', body)
                 stack = await portainer.updateStackString(
                     stackID,
-                    endpointID,
+                    inputs.endpointID,
                     body
                 )
                 // console.log('stack:', stack)
@@ -41860,23 +41830,27 @@ const Portainer = __nccwpck_require__(1055)
             } else {
                 core.info('Stack NOT Found - Deploying NEW Stack')
                 const body = {
-                    name,
+                    name: inputs.name,
                     swarmID,
                     stackFileContent,
-                    env,
+                    env: newEnv,
                 }
                 // console.log('body:', body)
-                stack = await portainer.createStackString(endpointID, body)
+                stack = await portainer.createStackString(
+                    inputs.endpointID,
+                    body
+                )
                 // console.log('stack:', stack)
                 core.info(`Deployed Stack: ${stack.Id}: ${stack.Name}`)
             }
         }
 
+        // Set Outputs
         core.setOutput('stackID', stack.Id)
         core.setOutput('swarmID', swarmID)
-        core.setOutput('endpointID', endpointID)
+        core.setOutput('endpointID', inputs.endpointID)
 
-        core.info(`\u001b[32;1mFinished Success`)
+        core.info('✅ \u001b[32;1mFinished Success')
     } catch (e) {
         core.debug(e)
         console.log('response:', e.response?.data)
@@ -41905,6 +41879,52 @@ function getEnv(env_json, env_file) {
         }
     }
     return env
+}
+
+/**
+ * @function parseInputs
+ * @param {String} owner
+ * @param {String} repo
+ * @return {{
+ *   token: string,
+ *   url: string,
+ *   name: string,
+ *   file: string,
+ *   endpoint: string | undefined,
+ *   ref: string,
+ *   repo: string,
+ *   tlsskip: boolean,
+ *   prune: boolean,
+ *   pull: boolean,
+ *   type: string,
+ *   standalone: boolean,
+ *   env_json: string | undefined,
+ *   env_file: string | undefined,
+ *   username: string | undefined,
+ *   password: string | undefined,
+ *   fs_path: string | undefined
+ * }}
+ */
+function parseInputs(owner, repo) {
+    return {
+        token: core.getInput('token', { required: true }),
+        url: core.getInput('url', { required: true }),
+        name: core.getInput('name', { required: true }),
+        file: core.getInput('file', { required: true }),
+        endpoint: core.getInput('endpoint'),
+        ref: core.getInput('ref') || github.context.ref,
+        repo: core.getInput('repo') || `https://github.com/${owner}/${repo}`,
+        tlsskip: core.getBooleanInput('tlsskip'),
+        prune: core.getBooleanInput('prune'),
+        pull: core.getBooleanInput('pull'),
+        type: core.getInput('type'),
+        standalone: core.getBooleanInput('standalone'),
+        env_json: core.getInput('env_json'),
+        env_file: core.getInput('env_file'),
+        username: core.getInput('username'),
+        password: core.getInput('password'),
+        fs_path: core.getInput('fs_path'),
+    }
 }
 
 module.exports = __webpack_exports__;
